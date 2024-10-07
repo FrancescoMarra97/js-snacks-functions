0cde8485d02e6789370a94ac70e20659b52c8afd
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-07 16:34:17 +0200

    es 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,9 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function findVowels(inputString){
+    const foundVowels=[]; //inizializzo un array vuoto per le vocali
+    const vowels = "aeiou" //definisco le vocali
+    for (let i = 0; i < inputString.length; i++) {
+        const element = inputString[i];
+        
+        //controllo se è una vocale
+       if (vowels.includes(element)) {
+        foundVowels.push(element)
+       }
+       return foundVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const result=findVowels(word);
+console.log(result);
+
 
 
 
