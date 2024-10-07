f7a6a87158191c950c62facffdeae4b2913928e8
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-07 15:42:04 +0200

    1 es

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,15 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 function getIinitial(listNames) {
+    const iniziali=[] //iniziallizzo l'array per le iniziali
+
     //scorrere nell'array 
     for (let i = 0; i < listNames.length; i++) {
         const element = listNames[i];
         //accedo al primo carattere della stringa
         const initial = element[0];
+        //invio le iniziali nell'array 
+        iniziali.push(initial);
     }
+    return iniziali;
 }
 
 // Invoca la funzione qui e stampa il risultato in console
+const result=getIinitial(names)
+console.log(result);
 
 
 
