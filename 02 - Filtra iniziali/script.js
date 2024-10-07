c0f9f1949c03d5b28fdf52f4249151d1ec172bf4
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-07 15:50:45 +0200

    dichiarazione della funzione

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,10 +1,15 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array 
+   contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function filterNamesByLetter(names, "A") {
+    const filteredNames[];
 
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
