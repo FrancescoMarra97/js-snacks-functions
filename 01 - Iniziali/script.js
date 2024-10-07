91287d6dd1968e5ebf0dc0a17933624995d55829
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-07 15:32:58 +0200

    ciclo for e accesso alle iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -4,7 +4,14 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getIinitial(listNames) {
+    //scorrere nell'array 
+    for (let i = 0; i < listNames.length; i++) {
+        const element = listNames[i];
+        //accedo al primo carattere della stringa
+        const initial = element[0];
+    }
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
